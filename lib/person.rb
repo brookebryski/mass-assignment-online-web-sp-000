eb5ab4763a54297ea75e77dc1b2eee05ba76a370
chic-state-0895 <brookeabryski@gmail.com>
2020-09-17 18:17:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,9 +5,9 @@ attr_accessor  :name, :birthday, :hair_color, :eye_color, :height,
   :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize (name, birthday, hair_color, eye_color, height, weight, handed, complexion, t_shirt_size, wrist_size, glove_size, pant_length, pant_width
-    @name = name 
-    @birthday = birthday 
-    @hair_color = hair_color 
+    @name = name
+    @birthday = birthday
+    @hair_color = hair_color
     @eye_color = eye_color
     @height = height
     @weight = weight
@@ -19,5 +19,5 @@ attr_accessor  :name, :birthday, :hair_color, :eye_color, :height,
     @pant_length = pant_length
     @pant_width = pant_width
   end
-  
+
 end
